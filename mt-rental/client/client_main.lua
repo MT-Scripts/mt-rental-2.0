0b2388d24591293bd03966c61d90caeee08635ee
Marttins <89866234+Marttins011@users.noreply.github.com>
2022-12-13 19:55:54 +0000

    Fix at spot name

--- mt-rental/client/client_main.lua
+++ mt-rental/client/client_main.lua
@@ -5,7 +5,6 @@ local Vehicle = nil
 CreateThread(function()
     for k, v in pairs(Config.RentalSpots) do
         local PedLoc = v.PedCoords
-        local Spot = v.SpotName
         RequestModel(GetHashKey(v.PedModel))
         while not HasModelLoaded(GetHashKey(v.PedModel)) do
             Wait(1)
@@ -28,7 +27,7 @@ CreateThread(function()
                     icon = "fas fa-clipboard",
                     label = v.LabelSpotName,
                     action = function()
-                        TriggerEvent('mt-rental:client:RentalMenu', Spot, v.VehicleSpawnLocation)
+                        TriggerEvent('mt-rental:client:RentalMenu', v.SpotName, v.VehicleSpawnLocation)
                     end
                 },
             },
